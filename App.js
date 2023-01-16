ac809531c93102ff287d0967d487dead265a0771
AlexBalew <balew48@gmail.com>
2023-01-16 13:16:28 +0300

    Basic setup & functionality

--- App.js
+++ App.js
@@ -1,11 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import Loading from './Loading';
+import { Alert, StyleSheet, Text, View } from 'react-native';
+import { API_KEY } from '@env'
+import * as Location from 'expo-location';
+import axios from 'axios';
 
 export default function App() {
+  const [coords, setCoords] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const getWeather = async (latitude, longitude) => {
+    const { data } = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`)
+    return data
+  }
+
+  useEffect(() => {
+    (async () => {
+      setIsLoading(true)
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied')
+        setIsLoading(false)
+        return;
+      }
+
+      try {
+        const { coords: { latitude, longitude } } = await Location.getCurrentPositionAsync()
+        const { data } = await getWeather(latitude, longitude)
+        setCoords(data)
+        setIsLoading(false)
+      } catch (error) {
+        setErrorMsg('Something went wrong...')
+        Alert.alert(error)
+        setIsLoading(false)
+      }
+    })();
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {
+        errorMsg
+          ?
+          <Text style={styles.text}>{errorMsg}</Text>
+          :
+          <Text style={styles.text}>{coords}</Text>
+      }
     </View>
   );
 }
@@ -13,8 +58,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: '#FDF6AA',
   },
-});
+  text: {
+    color: '#2c2c2c',
+    fontSize: 30,
+  },
+})
